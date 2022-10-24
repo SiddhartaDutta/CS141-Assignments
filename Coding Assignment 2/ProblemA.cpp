10e44a5bcd094242d03109257325961094aa4bfc
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-10-23 22:27:03 -0700

    Algorithm works with first test

--- Coding Assignment 2/ProblemA.cpp
+++ Coding Assignment 2/ProblemA.cpp
@@ -14,24 +14,30 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> temp;
         a.push_back(temp);
-        cout << "read: " << i << endl;
+        //cout << "read: " << i << endl;
     }
 
-    for(auto n : a){
+    while(a.size() > 1){
         // sort
         sort(a.begin(), a.end());
+        for(int i = 0; i < a.size(); i++){
+            cout << " " << a.at(i);
+        }
+        cout << endl;
 
         // increment count
         temp = a.at(0) + a.at(1);
         count += temp * 2;
+        cout << "addition: " << temp*2 << endl;
 
         // erase used elements
-        a.erase(a.begin(), a.begin() + 1);
+        a.erase(a.begin());
+        a.erase(a.begin());
 
         // push new element
         a.push_back(temp);
 
-        cout << "stuck\n";
+        cout << "size: " << a.size() << endl;
     }
     
     cout << count;
