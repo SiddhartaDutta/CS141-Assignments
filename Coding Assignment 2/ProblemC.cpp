0629bbc20d222e183420ef2fd4a9a17c42530003
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-11-08 18:38:18 -0800

    Regular commit

---
+++ Coding Assignment 2/ProblemC.cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+/*
+    1. Get money and candle values
+    2. Greedy choice: Pick the largest valued candle you can as many times as you can
+*/
+
+int main(){
+
+    int money, candles[9], temp;
+    cin >> money;
+
+    for(int i = 0; i < 9; i++){
+        cin >> temp;
+        candles[i] = temp;
+    }
+
+    
+}
